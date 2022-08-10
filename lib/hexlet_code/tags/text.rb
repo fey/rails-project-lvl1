5a90196ca647c52a97d949f6af170686591c91c4
feycot <feycot@gmail.com>
2022-08-11 02:20:32 +0500

    refactor

--- lib/hexlet_code/tags/text.rb
+++ lib/hexlet_code/tags/text.rb
@@ -3,6 +3,13 @@
 class HexletCode::Tags::Text
   def self.build(tag_data)
     value = tag_data[:value]
-    HexletCode::Tag.build('textarea', name: tag_data[:name], cols: 20, rows: 40) { value }
+
+    attrs = {
+      name: tag_data[:name]
+      cols: 20,
+      rows: 40
+    }.merge(tag_data.except(:value))
+
+    HexletCode::Tag.build('textarea', attrs) { value }
   end
 end
