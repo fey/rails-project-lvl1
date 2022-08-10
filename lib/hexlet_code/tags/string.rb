bd1832cc26c0841716d39c55193cd65e5b8fe1b3
feycot <feycot@gmail.com>
2022-08-11 02:18:51 +0500

    fix lint

--- lib/hexlet_code/tags/string.rb
+++ lib/hexlet_code/tags/string.rb
@@ -4,7 +4,7 @@ class HexletCode::Tags::String
   def self.build(tag_data)
     value = tag_data[:value]
 
-    attrs = { name: tag_data[:name], type: 'text', value: value}.compact
+    attrs = { name: tag_data[:name], type: 'text', value: value }.compact
 
     HexletCode::Tag.build('input', attrs)
   end
